f4ae338447a08fbbe437b17066bdd2b3cf2b874c
AlexZ <schuric@me.com>
2026-09-07 22:50:06 +0200

    PWA: sw.js

---
+++ app/sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'tagesdoku-v1';
+const SHELL = [
+  './', './index.html', './manifest.webmanifest',
+  './Tagesdokumentation_Ueberstunden_Rufdienst_ausfuellbar.pdf',
+  './icon-192.png', './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf-lib/1.17.1/pdf-lib.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c =>
+    Promise.all(SHELL.map(u => c.add(u).catch(() => null)))
+  ).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => hit))
+  );
+});
